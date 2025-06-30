e16ef3a30e0e0a0dc4ef46ba99589c1d9a3c2283
kollurvi <sagar.kolluri@gmail.com>
2025-06-30 06:05:48 +0530

    Annotations

--- src/main/java/com/springcore/annotation/bean/NotificationManager.java
+++ src/main/java/com/springcore/annotation/bean/NotificationManager.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificationManager {
     @Autowired
-    @Qualifier("emailNotificationMessage")
+    //@Qualifier("emailNotificationMessage")
     //@Qualifier("smsNotificationMessage")
     private NotificationMessage notificationMessage;
 
