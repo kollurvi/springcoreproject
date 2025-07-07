c269fcf805e1a2ff3f5dbcf968f943b212002dfb
emexo <info@emexotechnologies.com>
2025-07-07 06:20:42 +0530

    deleted the spring dao

--- src/main/java/com/springcore/springdao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.springcore.springdao;
-
-import java.util.List;
-
-public interface UserDAO {
-    void save(User user);
-    List<User> findAll();
-    User findById(int id)throws UserNotFoundException;
-}
